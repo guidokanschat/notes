c094e3e1e560d3304ba64660af7e10114564461d
Guido Kanschat <dr.guido.kanschat@gmail.com>
2019-06-03 09:15:38 +0200

    more on matrices

---
+++ numerik/code/backsub.py
@@ -0,0 +1,9 @@
+def backward_substitution(A,b):
+    (m,n) = A.shape
+    x = np.zeros(n)
+    for i in range(n-1,-1):
+      x[i] = b[i]
+      for j in range(i+1,n):
+        x[i] -= A[i,j]*x[j]
+      x[i] /= A[i,i]      
+    return x
