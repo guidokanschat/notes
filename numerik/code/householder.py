b3c928bbfd0ee8886012cecf07631d26c0230250
Guido Kanschat <dr.guido.kanschat@gmail.com>
2021-07-06 13:14:22 +0200

    Householder-Algorithmus

--- numerik/code/householder.py
+++ numerik/code/householder.py
@@ -13,6 +13,15 @@ def householder_apply (u,x):
   for i in range(0,n[0]):
     x[i] -= 2*s*u[i]
 
+def householder_qr (A):
+  (m,n) = A.shape
+  D = np.zeros(m)
+  for k in range(0,m-1):
+    D[k] = householder_compute(A[k:n,k])
+    for i in range (k+1,n):
+      householder_apply(A[k:n,k],A[k:n,i])
+  return D
+
 x = np.array([3.,4.,5.])
 print (x.shape)
 u = np.copy(x)
@@ -24,3 +33,9 @@ print(a)
 
 householder_apply (u,x)
 print(x)
+
+print("QR")
+A = np.array([[2.,-1.,0.],[-1.,2.,-1.],[0.,-1.,2.]])
+diag = householder_qr(A)
+print(A)
+print(diag)
