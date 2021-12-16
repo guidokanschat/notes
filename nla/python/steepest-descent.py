bcab90f3659d898e2fec6048674cb809065e00ae
Guido Kanschat <dr.guido.kanschat@gmail.com>
2021-12-16 13:23:51 +0100

    slides for minimal residual

--- nla/python/steepest-descent.py
+++ nla/python/steepest-descent.py
@@ -10,12 +10,3 @@ def steepest_descent(A,b,x,tol):
     alpha = (r*r)/(p*r)
     x += alpha*r
     r -= alpha*p
-
-dimension = n
-x = numpy.zeros(n)
-b = numpy.zeros(n)
-# fill b with right hand side
-# A suitable matrix object
-tol = np.sqrt(b*b)*1.e-12
-
-steepest_descent(A,b,x,tol)
