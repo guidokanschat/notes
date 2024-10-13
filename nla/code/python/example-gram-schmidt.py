11c3232aa8ed4d493f8891d3cd6c72402544aa4d
Guido Kanschat <dr.guido.kanschat@gmail.com>
2024-10-13 21:57:09 +0200

    finished first chapter for this year

--- nla/code/python/example-gram-schmidt.py
+++ nla/code/python/example-gram-schmidt.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def bmatrix(a):
+    """Returns a LaTeX bmatrix
+
+    :a: numpy array
+    :returns: LaTeX bmatrix as a string
+    """
+    if len(a.shape) > 2:
+        raise ValueError('bmatrix can at most display two dimensions')
+    lines = str(a).replace('[', '').replace(']', '').splitlines()
+    rv = [r'\begin{bmatrix}']
+    rv += ['  ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    rv +=  [r'\end{bmatrix}']
+    return '\n'.join(rv)
+  
+def sprod(u,v):
+  return np.dot(u,v)
+
 def gram_schmidt(v):
   (n,m) = v.shape
   for j in range(n):
@@ -20,12 +37,31 @@ def modified_gram_schmidt(v):
     v[:,j] /= norm
 
 
-n = 5
+n = 12
 
 #Build Hilbert matrix of size n
 hilbert = np.zeros((n,n))
 for i in range(n):
   for j in range(n):
     hilbert[i,j] = 1./(i+j+1.)
+# hilbert
 
-hilbert
+hilbert2 = hilbert.copy()
+
+gram_schmidt(hilbert)
+modified_gram_schmidt(hilbert2)
+
+np.set_printoptions(precision=1)
+#np.set_printoptions(suppress=True)
+
+gramian = np.zeros((n,n))
+for i in range(n):
+  for j in range(n):
+    gramian[i,j] = sprod(hilbert[:,j],hilbert[:,i])
+
+print(bmatrix(gramian))
+for i in range(n):
+  for j in range(n):
+    gramian[i,j] = sprod(hilbert2[:,j],hilbert2[:,i])
+
+print (bmatrix(gramian))
