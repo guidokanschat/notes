555c69f1e64385a2d65551d8cd4df17821ecbc87
Guido Kanschat <dr.guido.kanschat@gmail.com>
2025-06-01 23:13:06 +0200

    vector plots

---
+++ mobi2/gnuplot/logisticm.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+const unsigned int n=16;
+double scale = 1.6/n;
+
+void f(double x, double y)
+{
+  std::cout << x << '\t'
+	    << y << '\t'
+	    << (scale * 1.) << '\t'
+	    << (scale * y*(1-y)) << '\n';
+}
+
+int main()
+{
+  for (unsigned int i=0;i<=n;++i)
+    {
+      for (unsigned int j=0;j<=n;++j)
+	{
+	  f(2.*i/n,2.*j/n-2.);
+	}
+      std::cout << std::endl;
+    }
+}
